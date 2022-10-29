5ddb0ac5d34c34532f62fdf5c94c87648d6956fa
Максим <maxiim114@gmail.com>
2022-10-29 11:09:37 +0500

    Classes and Exceptions for third lab work

---
+++ src/Record.java
@@ -0,0 +1,11 @@
+public class Record {
+    protected long id;
+    protected String phoneNumber;
+    protected String name;
+
+    public Record(long id, String phoneNumber, String name){
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+}
